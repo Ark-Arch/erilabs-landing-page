b0a3343a28231935662b975cab8af00e9373b1ff
Ark-Arch <davidagbemuko.ad@gmail.com>
2025-06-01 07:51:57 +0100

    created a list of nav items

--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -1,6 +1,15 @@
 
+const navItems = [
+  {href: "#", text: 'Home'},
+  {href: "#services", text: 'Services'},
+  {href: "#about-us", text: 'About Us'},
+  {href: "#features", text: 'Features'},
+]
+
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className="absolute inset-x-0 top-0 z-50 py-6">
+      
+    </header>
   )
 }
