6cb87fc97cfc6b12a254f8fb906ab778c84fe8ba
Ark-Arch <davidagbemuko.ad@gmail.com>
2025-06-04 18:27:48 +0100

    fixed the call to action section

--- src/components/sections/CallToAction.tsx
+++ src/components/sections/CallToAction.tsx
@@ -11,15 +11,12 @@ export const CallToAction = () => {
           <div className="relative z-10 mx-auto text-center max-w-xl md:max-w-2xl py-8 md:py-10 px-6 md:px-8">
             <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-heading-1">
               
-              Quick Start you
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-violet-600">
-                own AI
-              </span>
-              Business
+              Quick! Optimise your <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-violet-600">
+               Very Own
+              </span> Business
             </h1>
             <Paragraph className="pt-10">
-              Leverage our AI-powered platform to revolutionize your digital
-              marketing efforts. Get data-driven insights and automation at your
+              Leverage our platform platform to revolutionize your industry - what it may be. Get data-driven insights and automation at your
               fingertips.
             </Paragraph>
             <div className="mx-auto max-w-md sm:max-w-xl pt-10 dark:text-white">
